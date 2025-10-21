dfbeef8f66011e3f917903d9bc19b7aa27970217
nfortier19@gmail.com <nfortier19@gmail.com>
2025-10-21 14:38:01 +0000

    op.margin

--- models/intermediate/int_orders_operational.sql
+++ models/intermediate/int_orders_operational.sql
@@ -1,19 +1,8 @@
-WITH ship AS (
-  SELECT
-    s.orders_id,
-    s.shipping_fee,
-    s.logCost AS log_cost,
-    CAST(s.ship_cost AS FLOAT64) AS ship_cost,
-    om.margin
-  FROM {{ source('raw', 'ship') }} AS s
-  LEFT JOIN {{ ref('int_orders_operational') }} AS om
-    ON s.orders_id = om.orders_id
-)
 SELECT
-  orders_id,
-  shipping_fee,
-  log_cost,
-  ship_cost,
-  margin,
-  (margin + shipping_fee - log_cost - ship_cost) AS operational_margin
-FROM ship
+om.*
+, s.shipping_fee
+, s.log_cost
+, s.ship_cost
+FROM {{ ref('int_orders_margin') }} AS om
+LEFT JOIN {{ ref('stg_raw_ship')}}AS s
+ON om.orders_id = s.orders_id
