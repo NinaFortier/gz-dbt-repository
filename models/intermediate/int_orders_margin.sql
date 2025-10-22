929801faed59b708b68d423902d7cbe9fc23395a
nfortier19@gmail.com <nfortier19@gmail.com>
2025-10-22 09:43:32 +0000

    commit

--- models/intermediate/int_orders_margin.sql
+++ models/intermediate/int_orders_margin.sql
@@ -5,5 +5,10 @@ SUM(revenue) as revenue,
 SUM (quantity) as quantity,
 SUM(purchase_price) as purchase_price,
 SUM (margin) as margin
+
+,ROUND(SUM (margin) + SUM(s.shipping_fee)- SUM(s.log_cost) + SUM( s.ship_cost),2) AS operational_margin
 FROM {{ ref('int_sales_margin') }}
-GROUP BY date_date, orders_id
+ LEFT JOIN {{ ref('stg_raw_ship') }} s
+     USING(orders_id)
+ GROUP BY orders_id, date_date
+ ORDER BY orders_id desc
