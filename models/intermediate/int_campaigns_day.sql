d69754d2ff87eef2fc2e2b980e19d759d85a1b40
nfortier19@gmail.com <nfortier19@gmail.com>
2025-10-22 13:37:43 +0000

    ok

--- models/intermediate/int_campaigns_day.sql
+++ models/intermediate/int_campaigns_day.sql
@@ -1,17 +1,8 @@
-WITH int_campaigns_day AS (
-    SELECT
-        date_date,
-        SUM(ads_cost) AS total_ads_cost,
-        SUM(impression) AS total_impressions,
-        SUM(click) AS total_clicks
-    FROM {{ ref('int_campaigns_day') }}
-    GROUP BY date_date
-)
-
 SELECT
-    date_date,
-    total_ads_cost,
-    total_impressions,
-    total_clicks
-FROM {{ ref('int_campaigns_day') }}
+date_date
+, SUM (ads_cost) AS ads_cost
+, SUM (impression) AS impression
+, SUM (click) AS click
+FROM {{ ref('int_campaigns') }}
+GROUP BY date_date
 ORDER BY date_date DESC
